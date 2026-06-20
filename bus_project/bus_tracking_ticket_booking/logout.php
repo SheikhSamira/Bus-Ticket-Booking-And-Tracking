15602c6d247f3fc844e9f8249e5104f804485f70
Sumaiya2065 <aktertasnim446@gmail.com>
2026-06-21 03:27:17 +0600

    my work

--- bus_project/bus_tracking_ticket_booking/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'config/database.php';
-
-// Destroy all session data
-session_destroy();
-
-// Redirect to home page
-redirect('index.html');
-?>
--- bus_project/bus_tracking_ticket_booking/uploads/db_connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "bus_tracking_bd");
-
-if (!$conn) {
-    die("Database connection failed: " . mysqli_connect_error());
-}
-?>
